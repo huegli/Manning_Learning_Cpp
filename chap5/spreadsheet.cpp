00fed731bdc227d2964e935d3fe323a20cec47ee
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2023-09-04 09:55:26 -0700

    spreadsheet with arrays

---
+++ chap5/spreadsheet.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+const int ROWS = 10;
+const int COLS = 10;
+
+int spreadsheet[ROWS][COLS]{};
+
+inline int &cell(char col, int row) { return spreadsheet[row - 1][col - 'A']; }
+
+const char SEPERATOR[] = "\t";
+
+int main()
+{
+    cell('D', 4) = 99;
+    cell('I', 10) = 30;
+    cell('F', 2) = 19;
+    cell('G', 5) = 93;
+    cell('A', 4) = 12;
+
+    std::cout << SEPERATOR;
+    for (int col = 0; col < COLS; col++)
+    {
+        std::cout << (char)('A' + col) << SEPERATOR;
+    }
+    std::cout << std::endl;
+
+    for (int row = 0; row < ROWS; row++)
+    {
+        std::cout << row + 1 << SEPERATOR;
+        for (int col = 0; col < COLS; col++)
+        {
+            std::cout << spreadsheet[row][col] << SEPERATOR;
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
